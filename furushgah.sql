5fe22e31e985033f0dc8516333a0da077ea144d1
9000000g <9000000g@gmail.com>
2016-08-29 12:31:12 +0430

    db updated

--- furushgah.sql
+++ furushgah.sql
@@ -23,12 +23,13 @@ DROP TABLE IF EXISTS `comments`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `comments` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `user` int(11) NOT NULL,
   `sale` int(11) NOT NULL,
   `body` text CHARACTER SET utf8,
   `date` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`user`,`sale`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=14 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -37,8 +38,33 @@ CREATE TABLE `comments` (
 
 LOCK TABLES `comments` WRITE;
 /*!40000 ALTER TABLE `comments` DISABLE KEYS */;
-INSERT INTO `comments` VALUES (1,7,'محصول عنی میباشد.','2016-08-13 12:48:16'),(1,10,'خیلی محصول جالبی هستش :)','2016-08-13 12:46:27'),(2,11,'اصلا ردیف نیست داداچ','2016-08-13 16:28:00');
+INSERT INTO `comments` VALUES (1,1,7,'محصول عنی میباشد.','2016-08-13 12:48:16'),(2,2,7,'خیلی محصول جالبی هستش :)','2016-08-13 12:46:27'),(3,5,7,'erfertert','2016-08-13 16:28:00'),(4,3,10,'اصلا ردیف نیست داداچ','2016-08-13 16:28:00'),(5,1,27,'خیلی ردیفه خدایی. بیاین بخریدش','2016-08-25 12:48:52'),(6,1,25,'این که واسه من بود!','2016-08-25 12:49:52'),(7,1,27,'چرا؟','2016-08-27 12:40:30'),(8,1,27,'سلام','2016-08-28 08:19:42'),(9,1,27,'داداچیا؟','2016-08-28 08:19:51'),(10,1,27,'چرا؟','2016-08-28 08:23:26'),(11,1,3,'خیلی ردیفه عزیزم :)','2016-08-28 08:43:43'),(12,1,26,'dasasd','2016-08-28 08:52:20'),(13,1,26,'عاااااااالی آقا عالییییی','2016-08-28 17:17:28');
 /*!40000 ALTER TABLE `comments` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `favorites`
+--
+
+DROP TABLE IF EXISTS `favorites`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `favorites` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user` int(11) NOT NULL,
+  `sale` int(11) NOT NULL,
+  PRIMARY KEY (`id`,`user`,`sale`)
+) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=latin1;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `favorites`
+--
+
+LOCK TABLES `favorites` WRITE;
+/*!40000 ALTER TABLE `favorites` DISABLE KEYS */;
+INSERT INTO `favorites` VALUES (7,1,21),(8,1,22),(11,1,1),(15,1,18),(16,1,7),(20,1,27),(21,1,26),(22,1,13),(23,1,10),(24,1,15),(26,1,14),(27,1,23),(28,1,17),(29,1,25);
+/*!40000 ALTER TABLE `favorites` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -61,7 +87,7 @@ CREATE TABLE `followers` (
 
 LOCK TABLES `followers` WRITE;
 /*!40000 ALTER TABLE `followers` DISABLE KEYS */;
-INSERT INTO `followers` VALUES (1,2),(1,3),(1,4),(2,1),(3,1),(4,1),(4,2),(4,3);
+INSERT INTO `followers` VALUES (1,1),(1,3),(1,4),(1,5),(1,8),(1,10),(2,1),(2,2),(3,1),(3,3),(3,7),(4,1),(4,2),(4,3),(4,4),(4,7),(5,5),(5,6),(6,6),(7,7),(8,8),(9,9),(10,10);
 /*!40000 ALTER TABLE `followers` ENABLE KEYS */;
 UNLOCK TABLES;
 
@@ -85,7 +111,7 @@ CREATE TABLE `sales` (
   `body` text CHARACTER SET utf8,
   `user` int(11) NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=18 DEFAULT CHARSET=latin1;
+) ENGINE=InnoDB AUTO_INCREMENT=28 DEFAULT CHARSET=latin1;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -94,32 +120,8 @@ CREATE TABLE `sales` (
 
 LOCK TABLES `sales` WRITE;
 /*!40000 ALTER TABLE `sales` DISABLE KEYS */;
-INSERT INTO `sales` VALUES (1,'پراید سال ۸۳',NULL,'2016-08-07 05:45:01','sale',NULL,3000000,NULL,NULL,'پراید مشتی خوبیه. خودم تازه خریدم. کلا ۱۰ کیلومتر کار کرده. خانومم یه بار کوبونده تو دیوار، ولی بدون رنگ دادم داییم درش آورد. خلاصه اگه بخری رفته تو پاچت.',1),(2,'خونه نقلی',NULL,'2016-08-08 07:35:20','rent','month',NULL,700000,'15000000','یه خونه نقلی داریم. آدرسش سر نواب، کوچه 42، پلاک 22',3),(3,'لپی مپی',3,'2016-08-08 14:25:30','sale','month',750000,NULL,NULL,'لپتاپ باسه خودم بوده :( اما بابا جونم میگه باید بلفوشمش :(',1),(4,'Dell 8900',3,'2016-08-09 06:28:24','sale','month',800000,NULL,NULL,'اینو عمم خریده داده به من. منم خوشم نیومد میخوام بفروشم.',1),(7,'کرایه کامپیوتر مشتی',3,'2016-08-09 12:58:12','rent','day',NULL,30000,'1000000','این کامپیوتر خیلی خفنه جاجی. بیا گرو بیگیر جون بچت ضرر نمیکنی',3),(8,'شرت خونی',3,'2016-08-09 13:33:57','rent','day',NULL,50000,'500000','توضیح موضیح یوخدی',1),(9,'دستگاه قند آرکری',4,'2016-08-13 11:05:48','sale','month',150000,NULL,NULL,'خیلی مشتیه به جون بچم. خدایی بیا بخر دیگه ناموسا. می‌خری؟',2),(10,'کامپیوتر خفن',3,'2016-08-13 11:34:06','sale','month',750000,NULL,NULL,'این کامپیوتر خیلی خوفه. همه چیش عالیه.',4),(11,'لپتاپ',3,'2016-08-13 16:27:37','rent','week',NULL,15000,'30000','این لپتاپ خوبیه',1),(13,'خربزه درشت مشدی',5,'2016-08-23 11:40:42','sale','month',1500,NULL,NULL,'خربزه درشت مشدی بدم فقط هزااااار',1),(14,'گومبا گومبا',2,'2016-08-23 11:55:06','sale','month',0,NULL,NULL,'این نسخه قدیمی تره',1),(15,'sad',1,'2016-08-23 11:56:29','sale','month',9,NULL,NULL,'asdasdasd',1),(16,'aasd',2,'2016-08-23 11:58:13','sale','month',2,NULL,NULL,'sdfdsfdsf',1),(17,'zxc',1,'2016-08-23 12:05:50','sale','month',12,NULL,NULL,'dsfsdfsd',1);
+INSERT INTO `sales` VALUES (1,'پراید سال ۸۳',1,'2016-08-07 05:45:01','sale',NULL,3000000,NULL,NULL,'پراید مشتی خوبیه. خودم تازه خریدم. کلا ۱۰ کیلومتر کار کرده. خانومم یه بار کوبونده تو دیوار، ولی بدون رنگ دادم داییم درش آورد. خلاصه اگه بخری رفته تو پاچت.',1),(2,'خونه نقلی',2,'2016-08-08 07:35:20','rent','month',NULL,700000,'15000000','یه خونه نقلی داریم. آدرسش سر نواب، کوچه 42، پلاک 22',3),(3,'لپی مپی',3,'2016-08-08 14:25:30','sale','month',750000,NULL,NULL,'لپتاپ باسه خودم بوده :( اما بابا جونم میگه باید بلفوشمش :(',10),(4,'Dell 8900',3,'2016-08-09 06:28:24','sale','month',800000,NULL,NULL,'اینو عمم خریده داده به من. منم خوشم نیومد میخوام بفروشم.',8),(7,'کرایه کامپیوتر مشتی',3,'2016-08-09 12:58:12','rent','day',NULL,30000,'1000000','این کامپیوتر خیلی خفنه جاجی. بیا گرو بیگیر جون بچت ضرر نمیکنی',3),(8,'شرت خونی',3,'2016-08-09 13:33:57','rent','day',NULL,50000,'500000','توضیح موضیح یوخدی',7),(9,'دستگاه قند آرکری',4,'2016-08-13 11:05:48','sale','month',150000,NULL,NULL,'خیلی مشتیه به جون بچم. خدایی بیا بخر دیگه ناموسا. می‌خری؟',2),(10,'کامپیوتر خفن',3,'2016-08-13 11:34:06','sale','month',750000,NULL,NULL,'این کامپیوتر خیلی خوفه. همه چیش عالیه.',4),(11,'لپتاپ',3,'2016-08-13 16:27:37','rent','week',NULL,15000,'30000','این لپتاپ خوبیه',1),(13,'خربزه درشت مشدی',5,'2016-08-23 11:40:42','sale','month',1500,NULL,NULL,'خربزه درشت مشدی بدم فقط هزااااار',4),(14,'گومبا گومبا',2,'2016-08-23 11:55:06','sale','month',0,NULL,NULL,'این نسخه قدیمی تره',1),(15,'sad',1,'2016-08-23 11:56:29','sale','month',9,NULL,NULL,'asdasdasd',1),(17,'zxc',1,'2016-08-23 12:05:50','sale','month',12,NULL,NULL,'dsfsdfsd',1),(18,'تست جدید',2,'2016-08-24 10:04:57','sale','month',3,NULL,NULL,'asdasdasd',1),(21,'34',1,'2016-08-24 10:08:36','sale','month',34,NULL,NULL,'234324',3),(22,'34',1,'2016-08-24 10:09:29','sale','month',34,NULL,NULL,'234324',5),(23,'sad',1,'2016-08-24 12:25:01','sale','month',123,NULL,NULL,'asdasd',6),(25,'sad',1,'2016-08-24 12:28:25','sale','month',123,NULL,NULL,'asdasd',6),(26,'عکس سنگین',1,'2016-08-24 12:29:43','sale','month',99,NULL,NULL,'سسسسشیشیسشسیشیس',5);
 /*!40000 ALTER TABLE `sales` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `sessions`
---
-
-DROP TABLE IF EXISTS `sessions`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `sessions` (
-  `id` varchar(255) NOT NULL,
-  `value` text CHARACTER SET utf8,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `sessions`
---
-
-LOCK TABLES `sessions` WRITE;
-/*!40000 ALTER TABLE `sessions` DISABLE KEYS */;
-INSERT INTO `sessions` VALUES ('9yhsw5ruaitthylo4cl4svs74nfghn_1470351986227','{}'),('aqlj0k7u8lb6poy5x2ym8ft9s170qd_1470749068785','{\"me\":{\"id\":1,\"mobile\":\"00989365586015\",\"alias\":\"امیر مومنیان\"}}'),('hgongvlry5p6iw3l66s0zb8nqjvnnu_1471105201446','{\"me\":{\"id\":1,\"mobile\":\"00989365586015\",\"alias\":\"امیر مومنیان\",\"following\":3,\"followers\":3}}'),('kupppgv4fv1cssoslfq8lo1ca70uz6_1470242261196','{\"me\":{\"id\":1,\"mobile\":\"00989365586015\",\"alias\":\"امیر مومنیان\",\"following\":3,\"followers\":3}}'),('zysnqck09xbw0smlnn5c3l6opszonz_1471105269210','{\"me\":{\"id\":2,\"mobile\":\"00989125529011\",\"alias\":\"حسن جوهرچی\",\"following\":1,\"followers\":2}}');
-/*!40000 ALTER TABLE `sessions` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
@@ -157,4 +159,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2016-08-23 17:01:36
+-- Dump completed on 2016-08-29 12:30:42
